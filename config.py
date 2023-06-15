6f3577b93360eba39fa26b1b9025f646cb910867
Luka <57399897+Erzangel@users.noreply.github.com>
2023-06-15 22:50:36 +0200

    fix: names of fields in secrets.json

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ import os
 def load_config():
   with open('./secrets.json', 'r') as f:
     secrets = json.load(f)
-  os.environ["DISCORD_API_KEY"] = secrets["AZURE_URI_GPT35TURBO"]
-  os.environ["OPENAI_API_KEY"] = secrets["AZURE_API_KEY"]
+  os.environ["DISCORD_API_KEY"] = secrets["DISCORD_API_KEY"]
+  os.environ["OPENAI_API_KEY"] = secrets["OPENAI_API_KEY"]
